b5a2c3c4a8aa0f8dcc76e046057e09a6bb767f7b
Aylin Hyumet <aylin.hyumet@telerik.com>
2017-05-31 20:08:51 +0300

    changeeeeeeeeeeeeeeeeeeeeeee

---
+++ Notepad.java
@@ -0,0 +1,9 @@
+package bg.pragmatic.notepads;
+
+public interface Notepad {
+	void createAndWriteOnNewPage(String title, String text);
+	void replaceText(int pageNumber, String text) throws PageNotFoundException;
+	void deleteText(int pageNumber) throws PageNotFoundException;
+	void deletePage(int pageNumber) throws PageNotFoundException;
+	void printAllPages();
+}
